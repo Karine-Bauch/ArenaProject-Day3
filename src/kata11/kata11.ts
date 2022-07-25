3ca41a7e563d831cef785795db43d8090403a072
Karine Bauch <karine.bauch.web@gmail.com>
2022-07-25 15:31:19 +0200

    kata11

---
+++ src/kata11/kata11.ts
@@ -0,0 +1,30 @@
+export type CallbackFn = (elem: unknown) => boolean;
+export type FilterFn = (arr: unknown[], cb: CallbackFn) => unknown[];
+
+export const isEqual: CallbackFn = (elem: unknown) => {
+    if (elem <= 5) {
+        return true;
+    } else {
+        return false;
+    }
+} 
+
+export const filter: FilterFn = (arr: unknown[], cb: CallbackFn) => {
+
+    // j'utilise le kata02 (arrLength)
+    let length: number = 0;
+    while (arr[length] !== undefined) {
+        length++;
+    };
+
+    let i: number = 0;
+    let filteredArr: unknown[] = [];
+    while (i <= length - 1) {
+        if (cb(arr[i])) {
+            filteredArr[i] = arr[i] // Je réutilise le kata06 (pushFn)
+        }
+        i++;
+    }
+
+    return filteredArr;
+}
