85ab07f560d951aeacbafb46b44b6fa4bbe6dab7
Karine Bauch <karine.bauch.web@gmail.com>
2022-07-25 11:49:28 +0200

    kata07

---
+++ src/kata07/kata07.test.ts
@@ -0,0 +1,11 @@
+
+import { reverseNumbers } from "./kata07";
+
+// Tu peux utiliser ce fichier pour tester ton code.
+// À toi de t'organiser pour tester comme tu le souhaites.
+// Mais attention, l'heure tourne...
+process.stdout.write(reverseNumbers([2, 3, 4, 5, 6]) + '\n');
+
+
+// PS : la commande suivante peut être utile...
+// tsc src/kata00/kata00.test.ts --outDir dist && node dist/kata00.test.js
---
+++ src/kata07/kata07.ts
@@ -0,0 +1,23 @@
+
+export type ReverseNumbersFn = (numbers: number[]) => number[];
+
+export const reverseNumbers: ReverseNumbersFn = (numbers: number[]) => {
+    
+    let length: number = 0;
+    while (numbers[length] !== undefined) {
+        length++;
+    };
+
+    let i:number = 0;
+    let iReverse: number = length - 1;
+    let reverseNumbersArr: number[] = [];
+    process.stdout.write(iReverse + '\n');
+    while (iReverse !== -1) {
+        reverseNumbersArr[i] = numbers[iReverse];
+        process.stdout.write(i + '\n');
+        process.stdout.write(iReverse + '\n');
+        i++;
+        iReverse--;
+    }
+    return reverseNumbersArr;
+}
