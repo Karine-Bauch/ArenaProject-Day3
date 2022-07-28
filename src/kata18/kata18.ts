bead3162694cea371738abdbbae40ee447fc94fb
Karine Bauch <karine.bauch.web@gmail.com>
2022-07-28 17:50:09 +0200

    kata18 - THE END ! - Ready for the next challenge, Kévin

--- src/kata18/kata18.ts
+++ src/kata18/kata18.ts
@@ -1,6 +1,56 @@
 
-export type DiffFn = (arrA: string[], arrB: string[]) => string[];
-export type ConcatNFn = (strA: string, strB: string, n: number) => string;
+export type CallbackFn = (elem: string) => boolean;
+export type CompareNFn = (strA: string, strB: string, n: number) => number;
+
+const arrLength = (arr: unknown[]) => {
+    let length: number = 0;
+    while (arr[length] !== undefined) {
+        length++;
+    };
+    return length;
+};
+
+const push = (arr: any, x: number, item: any) => {
+    arr[x] = item;
+    return arr;
+}
+
+const filter = (arr: string[], cb: CallbackFn) => {
+
+    let length: number = arrLength(arr);
+
+    let iArr: number = 0;
+    let iFilter: number = 0;
+    let filteredArr: string[] = [];
+    while (iArr <= length - 1) {
+        if (cb(arr[iArr])) {
+            push(filteredArr, iFilter, arr[iArr]); // Je réutilise le kata06 (pushFn)
+            iFilter++;
+        }
+        iArr++;
+    }
+
+    return filteredArr;
+};
+
+const find = (arr: string[], cb: CallbackFn) => {
+
+    let length: number = arrLength(arr);
+  
+    let i: number = 0;
+    let findedElem: string = '';
+    while (i <= length - 1) {
+        // process.stdout.write(cb(arr[i]) + '\n');
+        // process.stdout.write(arr[i] + '\n');
+  
+        if (cb(arr[i])) {
+            findedElem = arr[i]; // Je réutilise le kata06 (pushFn)
+            return findedElem;
+        }
+        i++;
+    }
+    return findedElem;
+  };
 
 const slice = (str: string, n: number) => {
     let i: number = 0;
@@ -24,10 +74,19 @@ const split = (str: string) => {
     return strArr;
 };
 
-export const concatN: ConcatNFn = (strA: string, strB: string, n: number) => {
-    // strA + slice(strB, index n-1)
-    let newStrB: string = slice(strB, n);
-    let strConcat: string = strA + newStrB;
+const diff = (strA: string[], strB: string[]) => {
+    return filter(strA, (itemA) => !find(strB, (itemB) => (itemA === itemB))) as string[];
+};
 
-    return strConcat;
+
+export const compareN: CompareNFn = (strA: string, strB: string, n: number) => {
+    
+    let arrSplicedStrA: string[] = split(slice(strA, n));
+    let arrSplicedStrB: string[] = split(slice(strB, n));
+
+    console.log(arrSplicedStrA);
+    console.log(arrSplicedStrB);
+
+    return arrLength(diff(arrSplicedStrA, arrSplicedStrB));
+
 };
