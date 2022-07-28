ef9db438c7bbaaa97e44a504face34c1d4e41e48
Karine Bauch <karine.bauch.web@gmail.com>
2022-07-28 16:47:39 +0200

    kata17

---
+++ src/kata17/kata17.ts
@@ -0,0 +1,33 @@
+
+export type DiffFn = (arrA: string[], arrB: string[]) => string[];
+export type ConcatNFn = (strA: string, strB: string, n: number) => string;
+
+const slice = (str: string, n: number) => {
+    let i: number = 0;
+    let arrStr: string[] = split(str);
+    let newStr: string = '';
+    
+    while (i <= n - 1) {
+        newStr += arrStr[i];
+        i++;
+    };
+    return newStr;
+};
+
+const split = (str: string) => {
+    let i: number = 0;
+    let strArr: string[] = [];
+    while (str[i] !== undefined) {
+        strArr[i] = str[i];
+        i++;
+    };
+    return strArr;
+};
+
+export const concatN: ConcatNFn = (strA: string, strB: string, n: number) => {
+    // strA + slice(strB, index n-1)
+    let newStrB: string = slice(strB, n);
+    let strConcat: string = strA + newStrB;
+
+    return strConcat;
+};
